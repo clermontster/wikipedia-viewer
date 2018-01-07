cb74c3fd3f07f834c9f4949d67edb449d607a68d
Jonnie Clermont <clermontster@gmail.com>
2018-01-07 15:43:02 -0600

    update builds for heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static('src'));
+app.use(express.static('dist'));
 
 app.get('/', (req, res) => {
   res.render('index.html');
